4ca082e7a68e1f88bae7eecac86aa51fc3862f50
mharizanova8703 <85656320+mharizanova8703@users.noreply.github.com>
2021-10-31 19:07:47 -0400

    Adding 4 files in a lib folder manager, Intern,Engineer, Employeejs

---
+++ index.js
@@ -0,0 +1,5 @@
+const inquirer = require("inquirer")
+const fs = require("fs");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
