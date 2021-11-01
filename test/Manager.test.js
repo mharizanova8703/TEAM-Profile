8f160901939af3b16f626b8d4569580eabe67f52
mharizanova8703 <85656320+mharizanova8703@users.noreply.github.com>
2021-11-01 16:10:27 -0400

    creating a test for a manager , expect manager.get officenumber()

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,1 +1,31 @@
+
 const Manager = require("../lib/Manager");
+
+test("Can create Manager object", () =>
+{
+    const manager = new Manager();
+    expect(typeof(manager)).toBe('object')
+})
+//Foo  value that can change
+ test ("Can set a office number via constructor arguments", () => {
+     const testOfficeNum = 100;
+    const manager = new Manager("Foo", 20, "mariya@qu.com", testOfficeNum);
+       expect(manager.officeNumber).toBe(testOfficeNum);
+  });
+  
+ test("Can get a roll via constuctor arguments", () => {
+
+const testValue = "Manager";
+    const manager = new Manager("Foo", 20, "mariya@qu.com", 100)
+expect(manager.getRole()).toBe(testValue);
+  });
+
+ test(" Can get office number via constuctor argumnets", () => {
+     const testOfficeNum = 100;
+    const manager = new Manager("Foo", 1, "mariya@qu.com", testOfficeNum);
+    expect(manager.getOfficeNumber()).toBe(testOfficeNum);
+  });
+ 
+
+
+
