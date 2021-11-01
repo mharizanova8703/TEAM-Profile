21373f4b7f2e1d11f091f29029854ada92597ce5
mharizanova8703 <85656320+mharizanova8703@users.noreply.github.com>
2021-10-31 22:08:56 -0400

    Creating Team member emty array, creating a function start prompt and promp manager  questions

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-const inquirer = require("inquirer")
-const fs = require("fs");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const Manager = require("./lib/Manager");
+const inquirer = require('inquirer')
+const fs = require('fs')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const Manager = require('./lib/Manager')
+const path = require('path')
+
+const teamMember = []
+function startPromp() {
+  // Promping Manager questions
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the managers name?',
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What is the managers id?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is the managers email?',
+    },
+    {
+      type: 'input',
+      name: 'officeNumber',
+      message: 'What is the managers email?',
+    },
+  ])
+}
