f35ac0de09db65999e1c9b299847d312ea5dd438
mharizanova8703 <85656320+mharizanova8703@users.noreply.github.com>
2021-11-01 12:29:51 -0400

    creating a class Engineer using the constructor name,id,email, and we use a super for Engineer we add a this.github insteade of office number

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const Intern = require('./lib/Intern')
 const Manager = require('./lib/Manager')
 const path = require('path')
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, 'output')
+const outputPath = path.join(OUTPUT_DIR, 'team.html')
 
-const render = require("./src/template.js");
+const render = require('./src/template.js')
 
 const teamMember = []
 function startPromp() {
-  // Promping Manager questions
+  // Creating function to Promp Manager questions
   inquirer
     .prompt([
       {
@@ -47,6 +47,32 @@ function startPromp() {
     })
 }
 function startTeam() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'command',
+        message: 'Would you like to add more team members?',
+        choices: ['Add an Engineer', 'Add an Intern', 'Create the team'],
+      },
+    ])
+    .then((answers) => {
+      statment = answers.command
+
+      switch (statement) {
+        case 'Add an Engineer':
+          prompEng()
+          break
+
+        case ' Add an Intern':
+          prompIntern()
+          break
+
+        case 'Create the team':
+          startTeam()
+          break
+      }
+    })
   // use inquirer to ask them what they want tot add to the team and use a switch function...depending on what they choose fire off that prompEnd,you prompManager, promptIntern
   // when you dont want to add anymore team members fire off the generateHTML function which will pass the teammembers array to another file where you will have to dynamically create bootstrap cards and append them to a dynamically created html
 }
@@ -84,43 +110,39 @@ function prompEng() {
 //creating a function to promp intern name,id,email,school name
 function prompIntern() {
   inquirer
-  .prompt([
-    {
-       type: "input",
-                name: "name",
-                message: "What is the Interns name?"
-    },
-     {
-                type: "input",
-                name: "id",
-                message: "What is the Interns id?"
-            },
-             {
-                type: "input",
-                name: "email",
-                message: "What is the Interns email?"
-            },
-            {
-                type: "input",
-                name: "school",
-                message: "What is the school name?"
-            }
-  ])
-  .then(answers => {
-            var {name, id, email, school} = answers;
-            var intern = Intern(name, id, email, school);
-
-        })
-
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the Interns name?',
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: 'What is the Interns id?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is the Interns email?',
+      },
+      {
+        type: 'input',
+        name: 'school',
+        message: 'What is the school name?',
+      },
+    ])
+    .then((answers) => {
+      var { name, id, email, school } = answers
+      var intern = Intern(name, id, email, school)
+    })
 }
-
 
 startPromp()
 
-function renderTeam(){
-  if(!fs.existsSync(OUTPUT_DIR)){
-    fs.mkdirSync(OUTPUT_DIR);
+function renderTeam() {
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR)
   }
   fs.writeFileSync(outputPath, render(teamMember), 'utf8')
-
 }
